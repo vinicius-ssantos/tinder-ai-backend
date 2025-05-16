fa4ea87dc81b9504a36a68f6d3ed6ff09b941714
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-05-15 22:06:58 -0300

    Baisc initializr aoo working state

---
+++ src/main/java/io/javabrains/tinder_ai_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package io.javabrains.tinder_ai_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running!";
+    }
+}
